558bbd3d570e8b44d954d9de8e6567b44044a12c
jahid-jhb <bd.info.jh@gmail.com>
2025-03-02 10:04:07 +0600

    change row css to tailwind css and added some section

--- script/main.js
+++ script/main.js
@@ -2,11 +2,16 @@ document.getElementById('random-theme')
     .addEventListener('click', function (event) {
         let color = '#';
 
-        for(let i = 0; i<6; i++){
+        for (let i = 0; i < 6; i++) {
             const hexLetters = "0123456789abcdef";
             const ranNum = Math.floor(Math.random() * 16);
 
             color += hexLetters[ranNum];
         }
         document.body.style.backgroundColor = color;
+    });
+
+document.getElementById('blog-page')
+    .addEventListener('click', function () {
+        window.location.href = 'pages/blog.html';
     });
