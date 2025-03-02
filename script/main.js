9a466a670bc8ffa9206a5b994e4fbb9f4c5cbe53
jahid-jhb <bd.info.jh@gmail.com>
2025-03-02 13:31:56 +0600

    added some new functionality and section

--- script/main.js
+++ script/main.js
@@ -15,3 +15,18 @@ document.getElementById('blog-page')
     .addEventListener('click', function () {
         window.location.href = 'pages/blog.html';
     });
+
+
+
+let todayDate = new Date;
+
+const dayName = ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+const monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+console.log(dayName[todayDate.getDay()]);
+console.log(monthName[todayDate.getMonth()]);
+console.log(todayDate.getDate());
+console.log(todayDate.getFullYear());
+console.log(todayDate.getHours());
+console.log(todayDate.getMinutes());
+console.log(todayDate.getSeconds());
